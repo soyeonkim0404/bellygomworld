280d5010400a27a88f78d4459c9d4d9e1e54dabc
KOAdmin_bs010630-at-967636457097 <jaepil.choi@emotion.co.kr>
2022-07-07 19:55:55 +0900

    테스트 5

--- shell/10.copySource.sh
+++ shell/10.copySource.sh
@@ -1,3 +1,14 @@
 rm -rf /app/world/*
 
 cp -R /app/world_deploy/dist/* /app/world/
+
+# 실행중인 node kill
+sudo pgrep -f node | xargs kill -9
+
+sudo rm -rf /app/world_deploy/node_modules
+
+sudo yarn install
+
+sudo yarn build
+
+sudo yarn start &
