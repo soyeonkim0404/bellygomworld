1060a7ac6aeffe45843f48a2bd5a46260afd2df8
HANSIN <hansin.cho@emotion.co.kr>
2022-08-08 12:24:42 +0900

    카이카스 연결 문구 추가

--- components/Header.vue
+++ components/Header.vue
@@ -135,6 +135,7 @@ export default {
       showLang: false,
       mobileGnbShow: false,
       showModal: false,
+      isConnect: false,
       linkList: [
         /*{
           url: "https://opensea.io/",
@@ -185,12 +186,22 @@ export default {
       });
     },
     async connectKaikas() {
-      try {
-        const klaytn = window.klaytn; //크롬에 깔린 카이카스 확장프로그램 안에는 klaytn 이 내장되어있다.
-        const accounts = await klaytn.enable(); //카이카스 로그인
-      } catch (err) {
-        alert("Kaikas 지갑이 설치되어 있지 않습니다.\n크롬에서 Kaikas 확장 프로그램을 설치해주세요!");
-        window.open("https://chrome.google.com/webstore/detail/kaikas/jblndlipeogpafnldhgmapagcccfchpi")
+      if (this.isConnect) {
+        if (window.confirm("지갑연결을 해제하시겠습니까?")) {
+          this.isConnect = false;
+        }
+      } else {
+        if (window.confirm("지갑연결을 하시겠습니까?")) {
+          try {
+            this.isConnect = true
+            const klaytn = window.klaytn; //크롬에 깔린 카이카스 확장프로그램 안에는 klaytn 이 내장되어있다.
+            const accounts = await klaytn.enable(); //카이카스 로그인
+          } catch (err) {
+            alert("Kaikas 지갑이 설치되어 있지 않습니다.\n크롬에서 Kaikas 확장 프로그램을 설치해주세요!");
+            window.open("https://chrome.google.com/webstore/detail/kaikas/jblndlipeogpafnldhgmapagcccfchpi")
+          }
+
+        }
       }
     },
   },
