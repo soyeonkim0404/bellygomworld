437aafd9c77c7b6ab7c86ed5c146f3bc57bfc3c3
jihyeon <jihyeon.kim@emotion.co.kr>
2022-08-16 00:45:24 +0900

    인터렉션 머지중

--- pages/m/index.vue
+++ pages/m/index.vue
@@ -1705,7 +1705,7 @@ export default {
   layout: "default",
   data() {
     return {
-      mintingModal: false,
+      mintingModal: true,
       showModal: false,
       showStoryModal: false,
       currIndi: 1,
