dfc8010e1f59036426a3ebf7023817146acfdec6
jihyeon <jihyeon.kim@emotion.co.kr>
2022-08-31 18:11:12 +0900

    커밋

--- shell/100.ApplicationStop.sh
+++ shell/100.ApplicationStop.sh
@@ -5,8 +5,8 @@ pgrep -f node | xargs kill -9
 
 # 02. 기존 배포 내역 정리
 rm -rf /app/world/*
-rm -rf /app/world_deploy/node_modules
+rm -rf /app/world_deploy/*
 
 # 03. index.html 복사
-cp -R /app/world_deploy/index.html /app/world/
+#cp -R /app/world_deploy/index.html /app/world/
 
